65a8c69a1a255e2bcbb2531d474b2fb8d527da1e
Ytkagus <Ytkagus321@gmail.com>
2023-02-13 00:21:10 +0500

    Versia5

--- Assets/Scripts/Gun1.cs
+++ Assets/Scripts/Gun1.cs
@@ -1,22 +1,72 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun1 : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 35;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
 
     public Camera fpsCam;
 
+    private float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
         {
--- Assets/Scripts/HealfBar.cs
+++ Assets/Scripts/HealfBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Bar : MonoBehaviour
+public class HealfBar : MonoBehaviour
 
 {
     [SerializeField] private DilationTimer dilationTimer;
